6d47a349b6daa6b5bddb7dcbd03188cea2c73b7c
Iain Collins <me@iaincollins.com>
2021-12-01 22:46:49 +0000

    Change log events back to import last 30 days by default

--- src/service/lib/events.js
+++ src/service/lib/events.js
@@ -191,7 +191,7 @@ const eventHandlers = {
 
 async function init ({
   lastActiveOnly = false,
-  minTimestamp = Date.now() - 10 * 24 * 60 * 60 * 1000 // Load last 30 days by default
+  minTimestamp = Date.now() - 30 * 24 * 60 * 60 * 1000 // Load last 30 days by default
 } = {}) {
   if (loadingComplete) return loadingStats() // If already run, don't run again
 
