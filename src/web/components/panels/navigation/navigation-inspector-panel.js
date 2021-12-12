17b6797505b58fcb194f6584e8eedc392c98f20d
Iain Collins <me@iaincollins.com>
2021-12-12 06:23:04 +0000

    Display distance to main star as int in inspector

--- src/web/components/panels/navigation/navigation-inspector-panel.js
+++ src/web/components/panels/navigation/navigation-inspector-panel.js
@@ -69,8 +69,8 @@ export default function NavigationInspectorPanel ({ systemObject, setSystemObjec
       <hr />
       {(systemObject.distanceToArrival && systemObject.distanceToArrival > 0) === true &&
         <div className='navigation-panel__inspector-section'>
-          <h4 className='text-primary'>Distance to arrival</h4>
-          <p className='text-info'>{systemObject.distanceToArrival} Ls</p>
+          <h4 className='text-primary'>Distance to main star</h4>
+          <p className='text-info'>{systemObject.distanceToArrival.toFixed(0)} Ls</p>
         </div>}
 
       {systemObject.type === 'Star' &&
