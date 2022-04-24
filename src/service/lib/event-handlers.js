d6dae3d432d6c5fa19240f96cebf325d7ede89a8
Iain Collins <me@iaincollins.com>
2022-04-24 04:30:09 +0100

    Some refinements to sending keys to control ship toggles

--- src/service/lib/event-handlers.js
+++ src/service/lib/event-handlers.js
@@ -6,15 +6,8 @@ const path = require('path')
 
 const pjXML = require('pjxml')
 const sendKeys = require('sendkeys-js')
-// const ioHook = require('iohook')
 
-// ioHook.start(true)
-
-// ioHook.on('keydown', (event) => {
-//   console.log('keydown', event)
-// })
-
-
+const TARGET_WINDOW_TITLE = 'Elite - Dangerous (CLIENT)'
 const KEYBINDS_DIR = path.join(os.homedir(), 'AppData', 'Local', 'Frontier Developments', 'Elite Dangerous', 'Options', 'Bindings')
 
 // Prefer Keybinds v4 file
@@ -96,28 +89,57 @@ class EventHandlers {
 
             const doc = pjXML.parse(keyBinds)
             const primaryElement = doc.select(`//${KEYBIND_XML_ELEMENT}/Primary`)
+            const primaryKey = convertEliteDangerousKeyBindingToInputKey(primaryElement?.attributes?.Key)
+            const primaryElementModifier = doc.select(`//${KEYBIND_XML_ELEMENT}/Primary/Modifier`)
             const secondaryElement = doc.select(`//${KEYBIND_XML_ELEMENT}/Secondary`)
-            let keyToUse
+            const secondaryKey = convertEliteDangerousKeyBindingToInputKey(secondaryElement?.attributes?.Key)
+            const secondaryElementModifier = doc.select(`//${KEYBIND_XML_ELEMENT}/Primary/Secondary`)
 
+            let keyToSend, modifierKey
             if (primaryElement?.attributes?.Device === 'Keyboard') {
-              keyToUse = primaryElement?.attributes?.Key.replace(/^Key_/, '')
-            } else if (secondaryElement?.attributes?.Device === 'Keyboard') {
-              keyToUse = secondaryElement?.attributes?.Key.replace(/^Key_/, '')
+              keyToSend = primaryKey
+              modifierKey = primaryElementModifier?.attributes?.Key.replace(/^Key_/, '')
             }
 
-            // TODO I'm sure there are more of these special cases...
-            if (keyToUse.toLowerCase() === 'semicolon') keyToUse = ';'
+            // If the primary key has a modifer, and the secondary key doesn't
+            // then we use the secondary key as the target key instead, as we
+            // don't currently support sending modifier keys.
+            if (modifierKey && primaryElement?.attributes?.Device === 'Keyboard') {
+              if (!secondaryElementModifier) {
+                  keyToSend = secondaryKey
+                  modifierKey = null
+                }
+            }
 
-            await sendKeys.activate('Elite - Dangerous (CLIENT)')
+            // If the secondary key is a single keystroke (with modifer) and the 
+            // primary key is not then prefer the secondary key as it's more
+            // likely to work as it won't have to rely on special key mapping.
+            if (primaryKey && secondaryKey && primaryKey.length > 1 && !secondaryElementModifier) {
+              keyToSend = secondaryKey
+              modifierKey = null
+            }
 
-            // Special keys (Home, Insert, F1) need to be wrapped in {} - e.g. {f1}
-            //await sendKeys.send(keyToUse?.length > 1 ? `{${keyToUse.toLowerCase()}}` : keyToUse)
+            // TODO Support Control and Alt modifiers
+            if (modifierKey?.toLowerCase()?.includes('shift')) modifierKey = 'shift'
 
-            console.log('keycode.codes[keyToUse]', keyToUse, keycode.codes[keyToUse.toLowerCase()])
-            KeyTap(keycode.codes[keyToUse.toLowerCase()]);
- 
+            const keyAsKeycode = convertKeyToKeycode(keyToSend) 
+            //const modifierKeyAsKeycode =  keycode.codes[modifierKey?.toLowerCase()]
+
+            //console.log('KEYBINDS_MAP[switchName]', switchName, KEYBINDS_MAP[switchName])
+            //console.log('Key', keyToSend, keyAsKeycode) // modifierKey, modifierKeyAsKeycode)
+
+            // Set Elite Dangerous as the active window
+            await sendKeys.activate(TARGET_WINDOW_TITLE)
+
+            // This doesn't work but the logic is correct (i.e. send modifer key
+            // down, then send the main key we want then release the modifer)
+            //if (modifierKeyAsKeycode === 'shift') KeyToggle(VK_LSHIFT, "down")
+            
+            KeyTap(keyAsKeycode)
+
+            //if (modifierKeyAsKeycode === 'shift') KeyToggle(VK_LSHIFT, "up")
+
             return true
-
 
           } catch (e) {
             console.error('ERROR_SENDING_KEY', switchName, e.toString())
@@ -130,8 +152,29 @@ class EventHandlers {
   }
 }
 
+function convertEliteDangerousKeyBindingToInputKey(rawKeyValue) {
+  const key = rawKeyValue.replace(/^Key_/, '').toLowerCase()
+  // TODO I'm very sure there are more of these special cases.
+  // I'm not sure why Elite does keybindings this way or how many
+  // special cases there are. I hope someone has reverse engineered it
+  // already.
+  if (key === 'semicolon') return ';'
+  if (key === 'rightbracket') return ']'
+  if (key === 'leftbracket') return '['
+  if (key === 'hash') return '#'
+  if (key === 'backslash') return '\\'
+  return key
+}
+
+function convertKeyToKeycode(key) {
+  let keyCode = keycode.codes[key?.toLowerCase()]
+  if (key === '#') keyCode = 35 // Should work, but doesn't :-(
+  if (key === '\\') keyCode = 92 // Should work, but doesn't :-(
+  return keyCode
+}
 
 
+// TODO Move all this out into a dedicated class and clean it up
 // https://stackoverflow.com/questions/41350341/using-sendinput-in-node-ffi
 const keycode = require("keycode");
 const ffi = require( "ffi-napi");
@@ -168,79 +211,72 @@ const KEYEVENTF_EXTENDEDKEY = 0x0001;
 const KEYEVENTF_KEYUP       = 0x0002;
 const KEYEVENTF_UNICODE     = 0x0004;
 const KEYEVENTF_SCANCODE    = 0x0008;
-//const MAPVK_VK_TO_VSC = 0;
+
+const VK_LSHIFT = 0xA0;
 
 class KeyToggle_Options {
-    asScanCode = true;
-    keyCodeIsScanCode = false;
-    flags = 0;
-    async = false; // async can reduce stutter in your app, if frequently sending key-events
+    asScanCode = true
+    keyCodeIsScanCode = false
+    flags = 0
+    async = false // async can reduce stutter in your app, if frequently sending key-events
 }
 
-let entry = new Input(); // having one persistent native object, and just changing its fields, is apparently faster (from testing)
-entry.type = INPUT_KEYBOARD;
-entry.time = 0;
-entry.dwExtraInfo = 0;
+let entry = new Input() // having one persistent native object, and just changing its fields, is apparently faster (from testing)
+entry.type = INPUT_KEYBOARD
+entry.time = 0
+entry.dwExtraInfo = 0
 function KeyToggle(keyCode, type = "down", options) {
-    const opt = Object.assign({}, new KeyToggle_Options(), options);
-    
+    const opt = Object.assign({}, new KeyToggle_Options(), options)
+
     // scan-code approach (default)
     if (opt.asScanCode) {
-        let scanCode = opt.keyCodeIsScanCode ? keyCode : ConvertKeyCodeToScanCode(keyCode);
-        let isExtendedKey = (scanCode & extendedKeyPrefix) == extendedKeyPrefix;
+        let scanCode = opt.keyCodeIsScanCode ? keyCode : ConvertKeyCodeToScanCode(keyCode)
+        let isExtendedKey = (scanCode & extendedKeyPrefix) == extendedKeyPrefix
 
-        entry.dwFlags = KEYEVENTF_SCANCODE;
+        entry.dwFlags = KEYEVENTF_SCANCODE
         if (isExtendedKey) {
-            entry.dwFlags |= KEYEVENTF_EXTENDEDKEY;
+            entry.dwFlags |= KEYEVENTF_EXTENDEDKEY
         }
 
-        entry.wVK = 0;
-        entry.wScan = isExtendedKey ? scanCode - extendedKeyPrefix : scanCode;
+        entry.wVK = 0
+        entry.wScan = isExtendedKey ? scanCode - extendedKeyPrefix : scanCode
     }
-    // (virtual) key-code approach
     else {
-        entry.dwFlags = 0;
-        entry.wVK = keyCode;
-        //info.wScan = 0x0200;
-        entry.wScan = 0;
+        entry.dwFlags = 0
+        entry.wVK = keyCode
+        entry.wScan = 0
     }
 
     if (opt.flags != null) {
-        entry.dwFlags = opt.flags;
+        entry.dwFlags = opt.flags
     }
     if (type == "up") {
-        entry.dwFlags |= KEYEVENTF_KEYUP;
+        entry.dwFlags |= KEYEVENTF_KEYUP
     }
 
     if (opt.async) {
         return new Promise((resolve, reject)=> {
             user32.SendInput.async(1, entry, arch === "x64" ? 40 : 28, (error, result)=> {
-                if (error) reject(error);
-                resolve(result);
-            });
-        });
+                if (error) reject(error)
+                resolve(result)
+            })
+        })
     }
-    return user32.SendInput(1, entry, arch === "x64" ? 40 : 28);
+
+    return user32.SendInput(1, entry, arch === "x64" ? 40 : 28)
 }
 
 function KeyTap(keyCode, opt) {
-    KeyToggle(keyCode, "down", opt);
-    KeyToggle(keyCode, "up", opt);
+  KeyToggle(keyCode, "down", opt)
+  KeyToggle(keyCode, "up", opt)
 }
 
-// function ConvertKeyCodeToScanCode(keyCode) {
-//     return user32.MapVirtualKeyExA(keyCode, 0, 0);
-// }
-
-//https://pastebin.pl/view/f527be0e
-// Scan-code for a char equals its index in this list. List based on: https://qb64.org/wiki/Scancodes, https://www.qbasic.net/en/reference/general/scan-codes.htm
-// Not all keys are in this list, of course. You can add a custom mapping for other keys to the function below it, as needed.
-let keys = "**1234567890-=**qwertyuiop[]**asdfghjkl;'`*\\zxcvbnm,./".split("");
+// https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-mapvirtualkeyexa
+// https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+// https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-sendinput
+// https://stackoverflow.com/questions/11438677/sendinput-for-keyboard-only-uppercase
 function ConvertKeyCodeToScanCode(keyCode) {
-  let keyChar = String.fromCharCode(keyCode).toLowerCase();
-  let result = keys.indexOf(keyChar);
-  console.assert(result != -1, `Could not find scan-code for key ${keyCode} (${keycode.names[keyCode]}).`)
-  return result;
+  return user32.MapVirtualKeyExA(keyCode, 0, 0)
 }
 
 module.exports = EventHandlers
