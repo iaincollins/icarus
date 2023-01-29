e2489917523c4f17c62de88f1423e59cd8b35073
Iain Collins <me@iaincollins.com>
2023-01-29 15:03:00 +0000

    Add method to allow simulation of messages

--- src/client/pages/test/index.js
+++ src/client/pages/test/index.js
@@ -1,7 +1,35 @@
+import { sendEvent } from 'lib/socket'
+
 export default function TestPage () {
   return (
-    <>
+    <div style={{padding: '1rem'}}>
       <h1>Test Page</h1>
-    </>
+      <hr/>
+      <h2>Simulate Events</h2>
+      <p>
+        Send a test event to all connected clients.
+      </p>
+      <input id='debug_event_name' defaultValue='Test' style={{textTransform: 'none'}}/>
+      <p>
+        <button onClick={() => {
+          sendEvent('testMessage', { name: document.getElementById('debug_event_name').value })
+        }}>Send Event</button>
+      </p>
+      <h2>Simulate Log Entry</h2>
+      <p>
+        Simulate an event triggered after an in-game log entry is fired.
+      </p>
+      <input id='debug_log_entry_name' defaultValue='Scan' style={{textTransform: 'none'}}/>
+      <p>
+        <button onClick={() => {
+          sendEvent('testMessage', { 
+            name: 'newLogEntry',
+            message: { 
+              event: document.getElementById('debug_log_entry_name').value }
+            }
+          )
+        }}>Simulate Log Entry</button>
+      </p>
+    </div>
   )
 }
--- src/service/lib/event-handlers.js
+++ src/service/lib/event-handlers.js
@@ -115,6 +115,11 @@ class EventHandlers {
         //     return null
         //   }
         // },
+        testMessage: ({name, message}) => {
+          console.log('sending test message', name)
+          // Method to simulate messages, intended for developers
+          if (name !== 'testMessage') broadcastEvent(name, message)
+        },
         testVoice: ({ voice }) => {
           // Escape voice name when passing as text as precaution to clean
           // input (NB: voice name argument is checked internally)
