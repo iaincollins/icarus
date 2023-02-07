362ee602faf5a79ab4577071fefdb8d395065c62
Iain Collins <me@iaincollins.com>
2023-02-07 09:41:14 +0000

    Update compass heading icon to resemble in game UI

--- src/client/components/panels/ship/ship-status/ship-instrumentation.js
+++ src/client/components/panels/ship/ship-status/ship-instrumentation.js
@@ -386,7 +386,7 @@ function NavigationInstrumentation ({ ship, cmdrStatus }) {
           display: panelActive && typeof cmdrStatus?.heading === 'number' ? ' block' : 'none'
         }}
         >
-          <i className='icarus-terminal-triangle-up' style={{position: 'absolute', top: '-.8rem', left: '-.15rem'}}/>
+          <i className='icarus-terminal-direction-heading' style={{position: 'absolute', top: '-1rem', left: '-.6rem', fontSize: '2rem'}}/>
         </div>
       </div>
       <div
